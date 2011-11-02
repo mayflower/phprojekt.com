197628469ce9987ef1d2bc2bff9645ba0debc86f
David Soria Parra <dsp@php.net>
2011-11-02 10:26:57 +0100

    Add pwiki tracking code

--- index.php
+++ index.php
@@ -240,5 +240,25 @@
   <script src="js/script.js"></script>
   <!-- end scripts-->
 
+<!-- Piwik -->
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ?
+"https://piwik.mayflower.de/" : "http://piwik.mayflower.de/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js'
+type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 3);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script>
+<noscript>
+<p>
+    <img src="http://piwik.mayflower.de/piwik.php?idsite=3" style="border:0" alt="" />
+</p>
+</noscript>
+<!-- End Piwik Tracking Code -->
+
 </body>
 </html>
